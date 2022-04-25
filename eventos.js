581782f8b14cc5c4fbe67855c73637495cbe947d
Farrush <rcarrenhot@gmail.com>
2022-04-25 11:34:08 -0300

    square

---
+++ eventos.js
@@ -0,0 +1,16 @@
+function clickMe() {
+    console.log("Fui clicado. :O")
+    console.log("AAAAAAAH")
+}
+function focusMe() {
+    console.log("Fui focado :(")
+}
+function blurMe() {
+    console.log("Saíram de mim ;-;")
+}
+function changeMe() {
+    console.log("Mudei C:")
+}
+function overMe() {
+    console.log("Mouse Aqui '-'")
+}
